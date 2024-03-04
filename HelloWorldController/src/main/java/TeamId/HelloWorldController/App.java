4d2c159ee5ae6543742758a3342be1107a95bc19
Sam Joseph Britto <Sam Joseph Britto@DESKTOP-OF89O90>
2024-03-04 13:52:26 +0530

    First simple springboot project

---
+++ HelloWorldController/src/main/java/TeamId/HelloWorldController/App.java
@@ -0,0 +1,13 @@
+package TeamId.HelloWorldController;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
